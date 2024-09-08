4bbfa0dd2498e30532d010303b591c82eff185f9
Lise Freja Jensen Petculescu <124042840+LisePetculescu@users.noreply.github.com>
2024-09-08 12:04:18 +0200

    binarySearch implemented

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 window.addEventListener("load", start);
+import { testBinarySearch } from "./BinarySearch/test_binarysearch.js";
 
 function start() {
   console.log("js is running");
+
+  testBinarySearch();
 }
